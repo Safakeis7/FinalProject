c9458070052d796e2cd257a46747481c96d84107
Safa Keis <keissa@mail.uc.edu>
2024-11-14 13:29:26 -0500

    Add project files.

---
+++ FinalProject/Models/Students.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Brithday { get; set; }
+        public string CollegeProgram {  get; set; }
+        public string YearInProgram { get; set; }
+
+        public Students() { }
+
+        public Students( int id, string name, DateTime brithday, string collegeProgram, string yearInProgram)
+        {
+            Id = id;
+            Name = name;
+            Brithday = brithday;
+            CollegeProgram = collegeProgram;
+            YearInProgram = yearInProgram;
+        }
+
+    }
+}
