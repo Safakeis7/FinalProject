8a1c1d936c9301e28bb6e072364c96436441af66
Vy Huynh <huynhvy@mail.uc.edu>
2024-11-19 13:43:55 -0500

    Update Program.cs (to use Swagger), created Cartoon model and controller

---
+++ FinalProject/Controllers/CartoonsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Data;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartoonsController : ControllerBase
+    {
+        private readonly FinalProjectContext _context;
+
+        public CartoonsController(FinalProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cartoons
+        [HttpGet]
+       public IActionResult GetCartoons()
+        {
+            return Ok(_context.Cartoon);
+        }
+
+        // GET: api/Cartoons/5
+        [HttpGet("{id}")]
+        public IActionResult GetCartoon(int id)
+        {
+            Cartoon cartoon = _context.Cartoon.Find(id);
+
+            if (cartoon == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cartoon);
+        }
+
+        // POST: api/Cartoons
+        [HttpPost]
+        public IActionResult PostCartoon(Cartoon cartoon)
+        {
+            _context.Cartoon.Add(cartoon);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        // PUT: api/Cartoons
+        [HttpPut]
+        public IActionResult PutCartoon(Cartoon cartoon)
+        {
+            try
+            {
+                _context.Entry(cartoon).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            { 
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        // DELETE: api/Cartoons/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCartoon(int id) {
+            Cartoon cartoon = _context.Cartoon.Find(id);
+            if (cartoon == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                _context.Cartoon.Remove(cartoon);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ FinalProject/Models/Cartoon.cs
@@ -0,0 +1,22 @@
+﻿namespace FinalProject.Models
+{
+    public class Cartoon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Studio { get; set; }
+        public DateOnly FirstAired { get; set; }
+        public string MainCharacter { get; set; }
+
+        public Cartoon() { }
+        public Cartoon(int id, string name, string studio, DateOnly firstAired, string mainCharacter)
+        {
+            Id = id;
+            Name = name;
+            Studio = studio;
+            FirstAired = firstAired;
+            MainCharacter = mainCharacter;
+        }
+
+    }
+}
