452c52642052a27a913cdcf7aa9247a6410b226d
Vy Huynh <huynhvy@mail.uc.edu>
2024-11-21 13:27:34 -0500

    fix typo for student.cs

--- FinalProject/Models/Students.cs
+++ FinalProject/Models/Students.cs
@@ -7,13 +7,13 @@ namespace FinalProject.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Brithday { get; set; }
+        public DateTime Birthday { get; set; }
         public string CollegeProgram {  get; set; }
         public string YearInProgram { get; set; }
 
         public Students() { }
 
-        public Students( int id, string name, DateTime brithday, string collegeProgram, string yearInProgram)
+        public Students( int id, string name, DateTime birthday, string collegeProgram, string yearInProgram)
         {
             Id = id;
             Name = name;
